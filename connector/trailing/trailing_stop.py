26429ebc64ec3d3413ef311b9583a2f61158055c
Valonloni1 <zejnulaivalon@gmail.com>
2025-06-21 15:59:31 +0200

    Shtova trailing_stop.py

---
+++ connector/trailing/trailing_stop.py
@@ -0,0 +1,7 @@
+# trailing_stop.py
+def apply_trailing_stop(position, trailing_pips=50):
+    """
+    Zhvendos SL në favor të pozicionit kur ai ecën në drejtimin e duhur.
+    """
+    # TODO: Logjika për trailing stop
+    pass
